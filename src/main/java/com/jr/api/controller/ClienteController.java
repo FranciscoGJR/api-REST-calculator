2ee1bc119de723404df09cbac0d890951ec20692
FranciscoGJR <franciscoogjr@gmail.com>
2023-03-05 16:25:05 -0300

    add POST http

--- src/main/java/com/jr/api/controller/ClienteController.java
+++ src/main/java/com/jr/api/controller/ClienteController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +13,7 @@ import com.jr.api.model.Cliente;
 import com.jr.api.repository.ClienteRepository;
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/cliente")
 public class ClienteController {
 
 
@@ -25,4 +27,11 @@ public class ClienteController {
     public List<Cliente> lista(){ 
         return clienteRepository.findAll();
 	}
+
+    //POST
+    @PostMapping
+    public Cliente create(@RequestBody Cliente cliente){
+        return clienteRepository.save(cliente);
+    }
+
 }
